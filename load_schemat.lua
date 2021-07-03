5360670fa5003f848ca35e6798b8becf11c37aa9
AFCMS <afcm.contact@gmail.com>
2021-07-04 00:40:06 +0200

    [code] remove debug line

--- load_schemat.lua
+++ load_schemat.lua
@@ -8,6 +8,5 @@ function db_manager.get_schemat(name)
 		return shemats[name]
 	end
 	shemats[name] = io.open(minetest.get_modpath(modname).."/sql/"..shem, "r"):read("*all")
-	minetest.log("error", dump(shemats[name]))
 	return shemats[name]
 end
