6527092e062f1c4b1dd716ef5769e40fa1a245bc
AFCMS <afcm.contact@gmail.com>
2021-07-04 00:39:20 +0200

    [code] fix crash

--- load_schemat.lua
+++ load_schemat.lua
@@ -7,6 +7,7 @@ function db_manager.get_schemat(name)
 	if shemats[name] then
 		return shemats[name]
 	end
-	shemats[name..":"..shem] = io.open(minetest.get_modpath(modname).."/sql/"..shem, "r"):read("*all")
+	shemats[name] = io.open(minetest.get_modpath(modname).."/sql/"..shem, "r"):read("*all")
+	minetest.log("error", dump(shemats[name]))
 	return shemats[name]
 end
